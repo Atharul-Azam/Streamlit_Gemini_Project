bb40e1ff1b150207b368e54abae793fd0cfef1ff
Md Atharul Azam Nisan <139434485+Atharul-Azam@users.noreply.github.com>
2026-04-16 21:11:11 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,90 +1,112 @@
 import streamlit as st 
-from api_calling import note_generator, audio_transcription,quiz_generator
+from api_calling import note_generator,audio_transcription,quiz_generator
 from PIL import Image
 
 
+
 #title
-st.title("Note Summary and Quiz Generator" , anchor = False)
-st.markdown("Upload upto 3 images to generate note summary and Quizes")
+st.title("Note Summary and Quiz Generator")
+st.markdown("Upload upto 3 images to generate Note summary and Quizzes")
 st.divider()
+
 
 with st.sidebar:
     st.header("Controls")
 
-    #Images
+    #image
     images = st.file_uploader(
-        "Upload the Photos of Your Notes" ,
-        type = ['jpg', 'jpeg', 'png'],
-        accept_multiple_files=True,
+        "Upload the photos of your note",
+        type=['jpg','jpeg','png'],
+        accept_multiple_files=True
     )
 
-    pil_images = []
+    pil_images =[]
 
     for img in images:
         pil_img = Image.open(img)
         pil_images.append(pil_img)
 
-    if  images:
-        if len(images) > 3:
-            st.error("Maximum Upload Reached")
+    if images:
+        if len(images)>3:
+            st.error("Upload at max 3 images")
         else:
             st.subheader("Uploaded images")
+            
             col = st.columns(len(images))
 
-            for i, img in enumerate(images):
+            
+
+            for i,img in enumerate(images):
                 with col[i]:
                     st.image(img)
 
-    #difficulty
+    #difficulty 
     selected_option = st.selectbox(
-        "Enter the Difficulty of Quiz",
-        ("Easy", "Medium", "Hard"),
-        index = None,
+        "Enter the difficulty of your quiz",
+        ("Easy","Medium","Hard"),
+        index = None
     )
-    
 
-    pressed = st.button("Click the button to initiate AI",
-              type = "primary",
-              
-              )
-    
+    pressed= st.button("Click the button to initiate AI",type="primary")
+
 
 if pressed:
     if not images:
-        st.error("You must upload atleast 1 image")
+        st.error("You must upload 1 image")
     if not selected_option:
-        st.error("you must select a difficulty")
+        st.error("You must select a difficulty")
+    
     if images and selected_option:
 
-        #note
+        #note 
 
-        with st.container(border = True):
-            st.subheader("Your Note")
+        with st.container(border=True):
+            st.subheader("Your note")
 
+            #the portion below will be replaced by API Call
 
             with st.spinner("AI is writing notes for you"):
-                generated_notes = note_generator(pil_images)    
+                generated_notes = note_generator(pil_images)
                 st.markdown(generated_notes)
 
-        #audio transcript
+
+
+
+
+        #Audio transcipt
         with st.container(border=True):
             st.subheader("Audio Transcription")
 
-            with st.spinner("AI is generating Audio :-"):
 
-                #clearing The Markdown
+
+            #the portion below will be replaced by API Call 
+            with st.spinner("AI is generating audio transcript for you"):
+
+                #clearing the markdown
+
                 generated_notes = generated_notes.replace("#","")
                 generated_notes = generated_notes.replace("*","")
-                generated_notes = generated_notes.replace("_","")
+                generated_notes = generated_notes.replace("-","")
                 generated_notes = generated_notes.replace("`","")
+
+
                 audio_transcript = audio_transcription(generated_notes)
                 st.audio(audio_transcript)
 
 
-
         #quiz
+
         with st.container(border=True):
             st.subheader(f"Quiz ({selected_option}) Difficulty")
-            with st.spinner("AI is Generating Quizes, Please wait..."):
-                quizzes = quiz_generator(pil_images, selected_option)
+
+            #the portion below will be replaced by API Call 
+
+            with st.spinner("AI is generating the quizzes"):
+                quizzes = quiz_generator(pil_images,selected_option)
                 st.markdown(quizzes)
+
+
+
+
+
+
